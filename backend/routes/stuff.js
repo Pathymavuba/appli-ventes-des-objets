8767a9d2016d475675711482d3376b787b511358
pathymavuba <mavubapathy@gmail.com>
2022-10-06 11:56:56 +0100

    verification de la validité du token

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const auth = require('../middleware/auth')
 const router = express.Router();
 const stuffControlers = require('../controlers/stuff');
 
 
-router.post('/', stuffControlers.creationObjets);
-router.get('/:id',stuffControlers.rechercheElementUnique);
-router.get('/', stuffControlers.rechercheElements)
-router.put('/:id',stuffControlers.modifierElement)
-router.delete('/:id',stuffControlers.supprimerElement)
+router.post('/',auth, stuffControlers.creationObjets);
+router.get('/:id',auth,stuffControlers.rechercheElementUnique);
+router.get('/',auth, stuffControlers.rechercheElements)
+router.put('/:id',auth,stuffControlers.modifierElement)
+router.delete('/:id',auth,stuffControlers.supprimerElement)
 
 
 
